fe4665337f9a4d1a591046c82c39496fc3d8149b
Bhargavi Jagana <bhargavi_jagana@epam.com>
2024-11-06 17:33:26 +0530

    first commit

---
+++ PageObjects/LoginPageLocators.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+
+namespace PageObjects
+{
+    public partial class LoginPage
+    {
+        private By by1 = By.Id("user-name");
+        private By by2 = By.Id("password");
+        private By by3 = By.Id("login-button");
+        private By productLabel = By.XPath("//*[@class='header_secondary_container']");
+        private By errorButton = By.XPath("//button[@class='error-button']");
+    }
+}
